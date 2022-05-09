440bb3b308381d9230c5fe3de608ca849ef4c892
Joey Yu <joey@itsjoeoui.com>
2022-05-09 12:27:05 -0400

    17 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        ntol_mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        combs = [""]
+        for d in digits:
+            combs = [x+y for x in combs for y in ntol_mapping[d]]
+        return combs
+# @lc code=end
