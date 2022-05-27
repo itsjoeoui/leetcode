b34e6e69d22ebcae859fdbaa5dcfc22937298ba0
Joey Yu <joey@itsjoeoui.com>
2022-05-26 23:34:12 -0400

    1342 Golang
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 1342.number-of-steps-to-reduce-a-number-to-zero.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1342 lang=golang
+ *
+ * [1342] Number of Steps to Reduce a Number to Zero
+ */
+
+// @lc code=start
+func numberOfSteps(num int) int {
+	step := 0
+	for num > 0 {
+		if num%2 == 0 {
+			num = num / 2
+		} else {
+			num -= 1
+		}
+		step += 1
+	}
+	return step
+}
+
+// @lc code=end
+
