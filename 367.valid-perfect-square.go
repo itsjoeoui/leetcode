7fec692df363bcd9b86119bad516bfefdf8b723f
Joey Yu <joey@itsjoeoui.com>
2022-06-04 20:15:59 -0400

    367 Golang
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 367.valid-perfect-square.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=367 lang=golang
+ *
+ * [367] Valid Perfect Square
+ */
+
+// @lc code=start
+func isPerfectSquare(num int) bool {
+	for i := 0; i < num/2+2; i++ {
+		if i*i == num {
+			return true
+		}
+		if i*i > num {
+			break
+		}
+	}
+	return false
+}
+
+// @lc code=end
+
