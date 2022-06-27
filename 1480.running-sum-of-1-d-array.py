b1a8f38d5f5aaf7f682801f0da6b117259748d33
Joey Yu <joey@itsjoeoui.com>
2022-06-26 21:17:26 -0400

    1480 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 1480.running-sum-of-1-d-array.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1480 lang=python3
+#
+# [1480] Running Sum of 1d Array
+#
+
+# @lc code=start
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + nums[i-1]
+        return nums
+# @lc code=end
