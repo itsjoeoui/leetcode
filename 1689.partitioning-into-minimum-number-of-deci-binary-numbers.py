bb9678a7d49f3a8a32a1b13548fc5bb179f068eb
Joey Yu <joey@itsjoeoui.com>
2022-06-26 20:26:56 -0400

    1689 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 1689.partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1689 lang=python3
+#
+# [1689] Partitioning Into Minimum Number Of Deci-Binary Numbers
+#
+
+# @lc code=start
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return max(n)
+# @lc code=end
