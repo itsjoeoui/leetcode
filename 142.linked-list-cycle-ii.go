094020073903c1c81a372c7280a2eedc58596d18
Joey Yu <joey@itsjoeoui.com>
2022-07-01 16:29:48 -0400

    142 Golang
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 142.linked-list-cycle-ii.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=142 lang=golang
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	// Floyd's cycle detection algorithm
+	s, f := head, head
+	for f != nil && f.Next != nil {
+		s, f = s.Next, f.Next.Next
+		if s == f {
+			for s != head {
+				s, head = s.Next, head.Next
+			}
+			return s
+		}
+	}
+	return nil
+
+}
+
+// @lc code=end
+
