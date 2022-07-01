1b30fd8decea110437ea61d4b1d2c159ba24881f
Joey Yu <joey@itsjoeoui.com>
2022-07-01 16:24:34 -0400

    142 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 142.linked-list-cycle-ii.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=142 lang=python3
+#
+# [142] Linked List Cycle II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                while head != slow:
+                    head, slow = head.next, slow.next
+                return slow
+        return None
+
+# @lc code=end
