fdcc1217a1173bc894cc009d23dd43f1cb59c850
Joey Yu <joey@itsjoeoui.com>
2022-07-02 13:06:23 -0400

    704 Golang
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 704.binary-search.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=704 lang=golang
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+func search(nums []int, target int) int {
+	lp, rp := 0, len(nums)-1
+	for lp <= rp {
+		mid := lp + (rp-lp)/2
+		if nums[mid] > target {
+			rp = mid - 1
+		} else if nums[mid] < target {
+			lp = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
+
+// @lc code=end
+
