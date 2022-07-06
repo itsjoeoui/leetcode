426adac5d1a269a866afe7d6e51c7822f9dcade4
Joey Yu <joey@itsjoeoui.com>
2022-07-06 14:53:28 -0400

    70 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 70.climbing-stairs.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
+# @lc code=end
