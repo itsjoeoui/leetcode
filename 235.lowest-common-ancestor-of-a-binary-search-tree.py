5a55adbbf93d30a16d7138dac692c7fc79ede7e1
Joey Yu <joey@itsjoeoui.com>
2022-07-06 17:04:15 -0400

    235 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=235 lang=python3
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
+# @lc code=end
