00c43da5f6a22641260ea588368bfff3266d576a
Joey Yu <joey@itsjoeoui.com>
2022-07-06 17:09:53 -0400

    235 Golang
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=235 lang=golang
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}
+
+// @lc code=end
+
