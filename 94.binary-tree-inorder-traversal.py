20a9b8a2c63865df0e0ff80b9d8560717b6b71b5
Joey Yu <joey@itsjoeoui.com>
2022-07-24 14:50:23 -0400

    94 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 94.binary-tree-inorder-traversal.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
+# @lc code=end
+
