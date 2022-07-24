5d9892e971b410817ad03c3a8ce8f61536d355db
Joey Yu <joey@itsjoeoui.com>
2022-07-24 12:25:16 -0400

    269 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 268.missing-number.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return (1+n)*n // 2 - sum(nums)
+# @lc code=end
