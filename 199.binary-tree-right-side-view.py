daefa62a1926baffe43b30df4a69a7854993c252
Joey Yu <joey@itsjoeoui.com>
2022-07-27 19:44:00 -0400

    199 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 199.binary-tree-right-side-view.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=199 lang=python3
+#
+# [199] Binary Tree Right Side View
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        from collections import deque
+
+        queue = deque([root])
+        rtn = []
+
+        while len(queue) > 0:
+            rtn.append(queue[-1].val)
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+
+        return rtn
+# @lc code=end
