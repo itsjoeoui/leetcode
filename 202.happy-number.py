9f625e4124c2cb589ce00020da5f81b4686fa419
Joey Yu <joey@itsjoeoui.com>
2022-08-16 23:26:23 -0400

    202 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 202.happy-number.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=202 lang=python3
+#
+# [202] Happy Number
+#
+
+# @lc code=start
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n = sum([int(digit) ** 2 for digit in str(n)])
+        return True
+
+# @lc code=end
