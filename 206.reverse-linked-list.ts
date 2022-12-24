2b011ea73f57a9d8d1adc09247fecb6fe5edd299
Joey Yu <joey@itsjoeoui.com>
2022-12-24 16:28:04 -0500

    206 TypeScript
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 206.reverse-linked-list.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=206 lang=typescript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode = null;
+  let cur: ListNode = head;
+
+  while (cur != null) {
+    let next: ListNode = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
+  }
+  return prev;
+}
+// @lc code=end
