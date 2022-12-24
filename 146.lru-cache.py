75ace2e83522451a1c8e77d4b2d1c52d4906fc22
Joey Yu <joey@itsjoeoui.com>
2022-12-24 11:45:47 -0500

    146 Python
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 146.lru-cache.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=146 lang=python3
+#
+# [146] LRU Cache
+#
+
+# @lc code=start
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        from collections import OrderedDict
+        self.cap: int = capacity
+        self.cache: OrderedDict = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last = False)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# @lc code=end
