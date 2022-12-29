369268b3f9a33d4285df70c7e914118f1050b641
Joey Yu <joey@itsjoeoui.com>
2022-12-29 11:24:15 -0500

    98 Java
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 98.validate-binary-search-tree.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=98 lang=java
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root.left, root.val, Long.MIN_VALUE) && dfs(root.right, Long.MAX_VALUE, root.val);
+    }
+
+    public boolean dfs(TreeNode root, long maxVal, long minVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        return dfs(root.left, root.val, minVal) && dfs(root.right, maxVal, root.val);
+    }
+}
+// @lc code=end
