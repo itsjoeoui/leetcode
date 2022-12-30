66be9d7f017fc181c80381d67b5af507bcc38545
Joey Yu <joey@itsjoeoui.com>
2022-12-30 15:40:02 -0500

    236 Java
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=236 lang=java
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        if (right != null) {
+            return right;
+        }
+        return null;
+    }
+}
+// @lc code=end
+
