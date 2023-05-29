e451940881e70cf20cd7483591d34dddd225b5fa
Joey Yu <joey@itsjoeoui.com>
2023-05-28 22:30:48 -0400

    49 Python

---
+++ src/arrays_and_hashing/49.group-anagrams.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+# https://leetcode.com/problems/group-anagrams/description/
+#
+# algorithms
+# Medium (66.59%)
+# Likes:    15391
+# Dislikes: 442
+# Total Accepted:    2M
+# Total Submissions: 3M
+# Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+#
+# Given an array of strings strs, group the anagrams together. You can return
+# the answer in any order.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a
+# different word or phrase, typically using all the original letters exactly
+# once.
+#
+#
+# Example 1:
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# Example 2:
+# Input: strs = [""]
+# Output: [[""]]
+# Example 3:
+# Input: strs = ["a"]
+# Output: [["a"]]
+#
+#
+# Constraints:
+#
+#
+# 1 <= strs.length <= 10^4
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
+#
+#
+#
+
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        tracking = defaultdict(list)
+        for word in strs:
+            std = "".join(sorted(word))
+            tracking[std].append(word)
+        return tracking.values()
+
+
+# @lc code=end
