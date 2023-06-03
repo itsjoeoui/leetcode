c2371ba3e251508061316deca7eb00dba5e0dd4b
Joey Yu <joey@itsjoeoui.com>
2023-06-03 15:31:38 -0400

    141 C++

---
+++ src/linked_list/141.linked-list-cycle.cpp
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=141 lang=cpp
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (47.11%)
+ * Likes:    12357
+ * Dislikes: 1033
+ * Total Accepted:    2.1M
+ * Total Submissions: 4.3M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given head, the head of a linked list, determine if the linked list has a
+ * cycle in it.
+ *
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is
+ * connected to. Note that pos is not passed as a parameter.
+ *
+ * Return true if there is a cycle in the linked list. Otherwise, return
+ * false.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to
+ * the 1st node (0-indexed).
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to
+ * the 0th node.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the list is in the range [0, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * pos is -1 or a valid index in the linked-list.
+ *
+ *
+ *
+ * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ *
+ */
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
