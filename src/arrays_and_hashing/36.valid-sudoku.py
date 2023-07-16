aea6b1963d612f31b6122e249022e0c61c862700
Joey Yu <joey@itsjoeoui.com>
2023-07-16 14:57:38 -0400

    36 Python

---
+++ src/arrays_and_hashing/36.valid-sudoku.py
@@ -0,0 +1,114 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+# https://leetcode.com/problems/valid-sudoku/description/
+#
+# algorithms
+# Medium (57.81%)
+# Likes:    9113
+# Dislikes: 953
+# Total Accepted:    1.2M
+# Total Submissions: 2M
+# Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
+#
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be
+# validated according to the following rules:
+#
+#
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9
+# without repetition.
+#
+#
+# Note:
+#
+#
+# A Sudoku board (partially filled) could be valid but is not necessarily
+# solvable.
+# Only the filled cells need to be validated according to the mentioned
+# rules.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: board =
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: board =
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner
+# being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it
+# is invalid.
+#
+#
+#
+# Constraints:
+#
+#
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+#
+#
+#
+
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        return self.checkRow(board) and self.checkCol(board) and self.checkBox(board)
+
+    def check(self, numbers) -> bool:
+        numbers = list(filter(lambda n: n != ".", numbers))
+        return len(numbers) == len(set(numbers))
+
+    def checkRow(self, board) -> bool:
+        for row in board:
+            if not self.check(row):
+                return False
+        return True
+
+    def checkCol(self, board) -> bool:
+        for i in range(9):
+            if not self.check([board[j][i] for j in range(9)]):
+                return False
+        return True
+
+    def checkBox(self, board) -> bool:
+        for r in [0, 3, 6]:
+            for c in [0, 3, 6]:
+                if not self.check(
+                    [board[r + dr][c + dc] for dr in [0, 1, 2] for dc in [0, 1, 2]]
+                ):
+                    return False
+        return True
+
+
+# @lc code=end
